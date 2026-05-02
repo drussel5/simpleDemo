e60730d2c3a6b887a623a774ad7cbc78125415c8
drussel5 <russell.devon91@gmail.com>
2026-05-02 13:44:53 -0400

    Added dev application properties along with a test debug message

--- src/main/java/com/devon/simpleDemo/SimpleDemoApplication.java
+++ src/main/java/com/devon/simpleDemo/SimpleDemoApplication.java
@@ -1,14 +1,18 @@
 package com.devon.simpleDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleDemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SimpleDemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleDemoApplication.class, args);
-		System.out.println("Hello Spring Boot!");
+		logger.info("SimpleDemo application started");
+		logger.debug("SimpleDemo debug message");
 	}
 
 }
